74fe4d0723c326c0e1d882fc460bdbb8037cc742
ertkjern <orjanert@gmail.com>
2017-01-28 23:14:41 +0100

    header - footer + npm pacakges
    Added footer and header layout for the website

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,11 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
 
-@NgModule({
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component'; 
+import { FooterComponent } from './components/footer/footer.component'; 
+
+  @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
