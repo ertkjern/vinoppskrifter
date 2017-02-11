50299400c085c1eb3d3564cf5561fc48fcd19ce5
ertkjern <orjanert@gmail.com>
2017-02-11 16:41:42 +0100

    Register Component done. (no validation yet)

--- web/src/app/views/register/register.component.ts
+++ web/src/app/views/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AngularFire } from 'angularfire2';
+import * as firebase from 'firebase';
+ 
 
 @Component({
     providers: [],
@@ -6,7 +10,28 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: 'register.component.html'
 })
 export class RegisterComponent implements OnInit {
-    constructor() { }
+
+    errorMessage: string = ""; 
+
+    constructor(private router: Router, firebase: AngularFire) { }
 
     ngOnInit() { }
+
+    register(email: string, username: string, password: string){
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((user) => {
+            user.updateProfile({
+                displayName: username
+            });
+            this.navigateHome(); 
+        })
+        .catch((error) => {
+            console.log("Something went wrong when trying to register the user.")
+            this.errorMessage = error.message;
+        });
+    }
+
+    navigateHome(){
+        this.router.navigate(['/home']);
+    }
 }
